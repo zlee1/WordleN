843d3f79734683e49a5e9bbe10f5822f54aa22d2
zlee1 <zack.lee@valpo.edu>
2022-02-27 14:34:19 -0600

    added keyboard and fixed mobile bug

--- wordle.js
+++ wordle.js
@@ -4,6 +4,35 @@ var output = document.getElementById("length_lbl");
 var tbl = document.getElementById("game_tbl");
 var chosen = null;
 var words = null;
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+function handleButton(id){
+  document.activeElement.blur();
+  var cur_row = 0;
+  for(var i = 0; i < tbl.rows.length; i++){
+    if(!['rgb(221, 221, 221)','rgb(189, 241, 130)','rgb(255, 245, 133)'].includes(tbl.rows[i].cells[0].style.backgroundColor)){
+      cur_row = i;
+      break;
+    }
+  }
+
+  for(var j = 0; j < tbl.rows[cur_row].cells.length; j++){
+    if(tbl.rows[cur_row].cells[j].innerHTML == ""){
+      tbl.rows[cur_row].cells[j].innerHTML = id;
+      return null;
+    }
+  }
+}
+
+document.onkeyup = function(e) {
+  if(e.code == "Enter"){
+    button.click();
+  }else if(e.keyCode >= 65 && e.keyCode <= 90){
+    document.getElementById(e.code.toString().charAt(3)).click();
+  }else if(e.code == "Backspace"){
+    document.getElementById("back_btn").click();
+  }
+}
 
 url = "https://raw.githubusercontent.com/zlee1/WordleN/master/data/generated/word_set.txt"
 
@@ -52,8 +81,9 @@ dec_btn.addEventListener('click', () => {
 });
 
 function clear_board(id){
+  document.getElementById("answer").innerHTML = null;
   chosen = null;
-  guess.value = null;
+  //guess.value = null;
   button.value = "Guess";
   tbl = document.getElementById(id);
   for(var i = 0; i < tbl.rows.length; i++){
@@ -65,20 +95,24 @@ function clear_board(id){
     for(var j = 0; j < tbl.rows[i].cells.length; j++)
       tbl.rows[i].cells[j].style.backgroundColor = "white";
   }
+
+  for(var i = 0; i < alphabet.length; i++){
+    document.getElementById(alphabet[i]).style.backgroundColor = "white";
+  }
 }
 
 function check_guess_legal(guess,len){
-  return words[len.toString()].legal.includes(guess);
+  return words[len.toString()].legal.includes(guess.toLowerCase());
 }
 
 var button = document.getElementById("guess_btn");
 var guess = document.getElementById("guess_txt");
-
+/**
 guess.addEventListener("keyup", function(e) {
   if(e.code == "Enter"){
     button.click();
   }
-});
+});**/
 
 function get_info(guess){
   var colors = [];
@@ -91,6 +125,7 @@ function get_info(guess){
     if(guess[i] == chosen[i]){
       colors[i] = "green";
       used_letters.push(guess[i]);
+      document.getElementById(guess[i].toUpperCase()).style.backgroundColor = "#BDF182";
     }
   }
 
@@ -119,32 +154,64 @@ function get_info(guess){
     }
   }
 
+  for(var i = 0; i < colors.length; i++){
+    if(colors[i] == "yellow" && document.getElementById(guess[i].toUpperCase()).style.backgroundColor != "#BDF182"){
+      document.getElementById(guess[i].toUpperCase()).style.backgroundColor = "#FFF585";
+    }
+    if(colors[i] == "gray"){
+      document.getElementById(guess[i].toUpperCase()).style.backgroundColor = "#DDDDDD";
+    }
+  }
   return colors;
 }
 
 function set_colors(colors, row){
-  if(row == null){
-    var cur_row = 0;
-    for(var row = 0; row < tbl.rows.length; row++){
-      if(tbl.rows[row].cells[0].innerHTML == ""){
-        cur_row = row-1;
-        break;
-      }
+  var cur_row = 0;
+  for(var row = 0; row < tbl.rows.length; row++){
+    if(!['rgb(221, 221, 221)','rgb(189, 241, 130)','rgb(255, 245, 133)'].includes(tbl.rows[row].cells[0].style.backgroundColor)){
+      cur_row = row;
+      break;
     }
   }
 
   for(var i = 0; i < colors.length; i++){
     if(colors[i] == "green"){
-      tbl.rows[row].cells[i].style.backgroundColor = '#BDF182';
+      tbl.rows[cur_row].cells[i].style.backgroundColor = '#BDF182';
     }else if(colors[i] == "yellow"){
-      tbl.rows[row].cells[i].style.backgroundColor = '#FFF585';
+      tbl.rows[cur_row].cells[i].style.backgroundColor = '#FFF585';
     }else{
-      tbl.rows[row].cells[i].style.backgroundColor = '#DDDDDD';
+      tbl.rows[cur_row].cells[i].style.backgroundColor = '#DDDDDD';
     }
   }
 }
 
+document.getElementById("back_btn").addEventListener('click', () => {
+  var last_row = 0;
+  var last_col = 0;
+  for(var i = 0; i < tbl.rows.length; i++){
+    for(var j = 0; j < tbl.rows[i].cells.length; j++){
+      if(tbl.rows[i].cells[j].innerHTML != ""){
+        last_row = i;
+        last_col = j;
+      }
+    }
+  }
+  if(!['rgb(221, 221, 221)','rgb(189, 241, 130)','rgb(255, 245, 133)'].includes(tbl.rows[last_row].cells[last_col].style.backgroundColor)){
+    tbl.rows[last_row].cells[last_col].innerHTML = "";
+  }
+});
+
 button.addEventListener('click', () => {
+  var user_guess = "";
+  for(var i = 0; i < tbl.rows.length; i++){
+    if(!['rgb(221, 221, 221)','rgb(189, 241, 130)','rgb(255, 245, 133)'].includes(tbl.rows[i].cells[0].style.backgroundColor)){
+      for(var j = 0; j < tbl.rows[i].cells.length; j++){
+        user_guess = user_guess + tbl.rows[i].cells[j].innerHTML;
+      }
+      break;
+    }
+  }
+
   if(button.value == "Restart"){
     chosen = null;
     clear_board("game_tbl");
@@ -153,34 +220,29 @@ button.addEventListener('click', () => {
   }
 
   if(chosen == null){
-    chosen = choose_word(tbl.rows[0].cells.length);
+    chosen = choose_word(tbl.rows[0].cells.length).toUpperCase();
   }
 
-  if(guess.value == chosen){
-    set_colors(get_info(guess.value, null));
-    button.value = "Restart";
-  }
-
-  if(check_guess_legal(guess.value,tbl.rows[0].cells.length)){
+  if(check_guess_legal(user_guess,tbl.rows[0].cells.length)){
 
     var cur_row = 0;
     for(var row = 0; row < tbl.rows.length; row++){
-      if(tbl.rows[row].cells[0].innerHTML == ""){
+      if(!['rgb(221, 221, 221)','rgb(189, 241, 130)','rgb(255, 245, 133)'].includes(tbl.rows[row].cells[0].style.backgroundColor)){
         cur_row = row;
         break;
       }
     }
-    for(var i = 0; i < guess.value.length; i++){
-      tbl.rows[cur_row].cells[i].innerHTML = guess.value[i].toUpperCase();
+    for(var i = 0; i < user_guess.length; i++){
+      tbl.rows[cur_row].cells[i].innerHTML = user_guess[i].toUpperCase();
     }
-    set_colors(get_info(guess.value), cur_row);
+    set_colors(get_info(user_guess), cur_row);
 
-    if(cur_row == tbl.rows.length-1 || guess.value == chosen){
+    if(cur_row == tbl.rows.length-1 || user_guess == chosen){
       button.value = "Restart";
-      guess.value = chosen.toUpperCase();
+      document.getElementById("answer").innerHTML = chosen.toUpperCase();
     }
     else{
-      guess.value = null;
+      user_guess = null;
     }
 
   }
@@ -210,3 +272,83 @@ function deleteColumn(tblId)
 function choose_word(len){
   return words[len.toString()].answers[Math.floor(Math.random()*words[len.toString()].answers.length)];
 }
+
+
+document.getElementById("A").addEventListener('click', () => {
+  handleButton("A");
+});
+document.getElementById("B").addEventListener('click', () => {
+  handleButton("B");
+});
+document.getElementById("C").addEventListener('click', () => {
+  handleButton("C");
+});
+document.getElementById("D").addEventListener('click', () => {
+  handleButton("D");
+});
+document.getElementById("E").addEventListener('click', () => {
+  handleButton("E");
+});
+document.getElementById("F").addEventListener('click', () => {
+  handleButton("F");
+});
+document.getElementById("G").addEventListener('click', () => {
+  handleButton("G");
+});
+document.getElementById("H").addEventListener('click', () => {
+  handleButton("H");
+});
+document.getElementById("I").addEventListener('click', () => {
+  handleButton("I");
+});
+document.getElementById("J").addEventListener('click', () => {
+  handleButton("J");
+});
+document.getElementById("K").addEventListener('click', () => {
+  handleButton("K");
+});
+document.getElementById("L").addEventListener('click', () => {
+  handleButton("L");
+});
+document.getElementById("M").addEventListener('click', () => {
+  handleButton("M");
+});
+document.getElementById("N").addEventListener('click', () => {
+  handleButton("N");
+});
+document.getElementById("O").addEventListener('click', () => {
+  handleButton("O");
+});
+document.getElementById("P").addEventListener('click', () => {
+  handleButton("P");
+});
+document.getElementById("Q").addEventListener('click', () => {
+  handleButton("Q");
+});
+document.getElementById("R").addEventListener('click', () => {
+  handleButton("R");
+});
+document.getElementById("S").addEventListener('click', () => {
+  handleButton("S");
+});
+document.getElementById("T").addEventListener('click', () => {
+  handleButton("T");
+});
+document.getElementById("U").addEventListener('click', () => {
+  handleButton("U");
+});
+document.getElementById("V").addEventListener('click', () => {
+  handleButton("V");
+});
+document.getElementById("W").addEventListener('click', () => {
+  handleButton("W");
+});
+document.getElementById("X").addEventListener('click', () => {
+  handleButton("X");
+});
+document.getElementById("Y").addEventListener('click', () => {
+  handleButton("Y");
+});
+document.getElementById("Z").addEventListener('click', () => {
+  handleButton("Z");
+});
